0a034481ebb8322e16eba4cbda9b4ac3bf0ce0d1
Patrick Buck <pappybuck@hotmail.com>
2023-04-04 15:10:08 -0400

    Added styling

--- src/components/storyCard.tsx
+++ src/components/storyCard.tsx
@@ -1,24 +1,26 @@
 import { Story } from "@/story/storyloader";
 import { Dispatch, SetStateAction } from "react";
-
+import styles from "@/styles/storyCard.module.scss";
 
 export default function StoryCard( {story, setId } : { story: Story, setId: Dispatch<SetStateAction<number>> } ) {
     return (
-        <div>
+        <div className={styles.card}>
             <h1>{story.title}</h1>
-            <div className="button-container">
-                <p>
+            <div className={styles.card__container}>
+                <p className={styles.card__text}>
                     {story.description}
                 </p>
+                <div className={styles.card__buttonBox}>
                 {story.isEnding ?
-                (
-                    <button onClick={() => setId(1)}>Restart</button>
-                ) : (
-                    <>
-                        <button onClick={() => setId(story.leftid)}> {story.left} </button>
-                        <button onClick={() => setId(story.rightid)}> {story.right} </button>
-                    </>
-                )}
+                    (
+                        <button onClick={() => setId(1)}>Restart</button>
+                    ) : (
+                        <>
+                            <button onClick={() => setId(story.leftid)}> {story.left} </button>
+                            <button onClick={() => setId(story.rightid)}> {story.right} </button>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     )
